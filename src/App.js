f70d1f142f9cc9854d4cd7e4ffac4b07095b4f14
rrsrnc <rajeev7605@gmail.com>
2023-06-02 15:32:07 +0530

    debugging websocket address

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import {io} from 'socket.io-client'
 import CncInfo from './cncInfo.js'
 import Document from './documents.js'
 
-const socket = io.connect("wss://thermalcomepensationdashboard.onrender.com/socket.io");
+const socket = io.connect("http://localhost:5000/");
 function App() {
   // socket.on('timestamp', (data)=> {
   //   console.log(data.time)
