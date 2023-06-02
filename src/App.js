98b4ebcc40f79c47e698d606a8b8574d9f80e739
rrsrnc <rajeev7605@gmail.com>
2023-06-02 15:38:34 +0530

    cors eror

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import {io} from 'socket.io-client'
 import CncInfo from './cncInfo.js'
 import Document from './documents.js'
 
-const socket = io.connect("http://localhost:5000/");
+const socket = io.connect("https://thermalcomepensationdashboard.onrender.com/");
 function App() {
   // socket.on('timestamp', (data)=> {
   //   console.log(data.time)
